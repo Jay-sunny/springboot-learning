abae12e188688f1fb7344ae380f0349285a8ec5b
Jay-sunny <80574424@qq.com>
2020-05-09 18:19:34 +0800

    lea-build-hello启动类的注释修改

--- lea-build-hello/src/main/java/com/yang/lea/hello/LeaBuildHelloApplication.java
+++ lea-build-hello/src/main/java/com/yang/lea/hello/LeaBuildHelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Yang
  * @version 1.0
  * @date 2020-3-22 20:50
- * @description 启动类
+ * @description LeaBuildHello启动类
  * 组合注解：@SpringBootApplication 相当于@EnableAutoConfiguration和@ComponentScan。
  * 注解：@EnableAutoConfiguration 表示开启自动化配置。开启自动化配置过后会自动进行Spring和SpringMVC的配置。
  * 注解：@ComponentScan 表示进行包扫描。
