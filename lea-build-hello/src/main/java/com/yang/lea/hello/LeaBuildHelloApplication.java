9f60fdabf707b2b50225e8de8843aeeb75d93e4d
Jay-sunny <80574424@qq.com>
2020-05-09 17:16:29 +0800

    springboot-learning的pom文件修改

---
+++ lea-build-hello/src/main/java/com/yang/lea/hello/LeaBuildHelloApplication.java
@@ -0,0 +1,18 @@
+package com.yang.lea.hello;
+
+
+/**
+ * @author Yang
+ * @version 1.0
+ * @date 2020-3-22 20:50
+ * @description LeaBuildHelloApplication启动类
+ * 组合注解：@SpringBootApplication 相当于@EnableAutoConfiguration和@ComponentScan。
+ * 注解：@EnableAutoConfiguration 表示开启自动化配置。开启自动化配置过后会自动进行Spring和SpringMVC的配置。
+ * 注解：@ComponentScan 表示进行包扫描。
+ */
+@SpringBootApplication
+public class LeaBuildHelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeaBuildHelloApplication.class);
+    }
+}
